487072479f9fab6767d6489df52e97db72841e18
MateMar04 <mateo.marchisone@gmail.com>
2021-07-10 19:33:23 -0300

    Basic bot

--- main.py
+++ main.py
@@ -3,3 +3,18 @@ import os
 import requests
 
 client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print("{0.user}".format(client))
+
+@client.event
+async def on_message(message):
+    if message.author != client.user:
+        if message.content.startswith("&help"):
+           await message.channel.send("GUIA DE AYUDA")
+        return
+
+
+client.run("ODYzNTQyNDcxODA0MTkwNzUx.YOoamQ.wvtcb_JhmKoY_CYCdFWUkZQYmF0")
